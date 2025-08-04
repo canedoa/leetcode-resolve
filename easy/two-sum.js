49099ac6bbefb531f748f874e58e6d161c422706
canedoa <c.canedo.amaya96@gmail.com>
2025-08-03 22:37:11 -0600

    Segundo ejercicio menos eficiente pero se llega al mismo resultado

--- easy/two-sum.js
+++ easy/two-sum.js
@@ -1,14 +1,30 @@
+// function twoSum(nums, target) {
+//   const map = new Map();
+//   for (let i = 0; i < nums.length; i++) {
+//     const complement = target - nums[i];
+//     if (map.has(complement)) {
+//       return [map.get(complement), i];
+//     }
+//     map.set(nums[i], i);
+//   }
+// }
+
+// // Prueba
+// const resultado = twoSum([2, 7, 11, 15], 9);
+// console.log("Resultado:", resultado); // [0, 1]
+
+//Funcion menos eficiente pero llegamos al mismo resultado
+const nums = [2, 7, 11, 15];
+const target = 9;
+
 function twoSum(nums, target) {
-  const map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-    if (map.has(complement)) {
-      return [map.get(complement), i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
-    map.set(nums[i], i);
   }
 }
 
-// Prueba
-const resultado = twoSum([2, 7, 11, 15], 9);
-console.log("Resultado:", resultado); // [0, 1]
+console.log(twoSum(nums, target));
