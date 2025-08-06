ee68b62e11dc56c99fb16827c241e8fae736c9ba
canedoa <c.canedo.amaya96@gmail.com>
2025-08-06 10:45:49 -0600

    funcion más optima para generar el mismo resultado con hasmap

--- easy/two-sum.js
+++ easy/two-sum.js
@@ -1,21 +1,6 @@
-// function twoSum(nums, target) {
-//   const map = new Map();
-//   for (let i = 0; i < nums.length; i++) {
-//     const complement = target - nums[i];
-//     if (map.has(complement)) {
-//       return [map.get(complement), i];
-//     }
-//     map.set(nums[i], i);
-//   }
-// }
-
-// // Prueba
-// const resultado = twoSum([2, 7, 11, 15], 9);
-// console.log("Resultado:", resultado); // [0, 1]
-
 //Funcion menos eficiente pero llegamos al mismo resultado
 const nums = [2, 7, 11, 15];
-const target = 9;
+const target = 9; // realizar pruebas con 17 y 22
 
 function twoSum(nums, target) {
   for (let i = 0; i < nums.length; i++) {
@@ -28,3 +13,41 @@ function twoSum(nums, target) {
 }
 
 console.log(twoSum(nums, target));
+
+//forma más optima con hashmap
+
+const nums2 = [2, 7, 11, 15];
+const target2 = 9;
+
+function twoSum2(nums2, target2) {
+  const map = new Map();
+
+  for (let i = 0; i < nums2.length; i++) {
+    const num = nums2[i];
+    const complemento = target2 - num;
+
+    console.log(`👉 Iteración ${i}`);
+    console.log(`num = ${num}, complemento = ${complemento}`);
+    console.log(`map.has(${complemento}) = ${map.has(complemento)}`);
+
+    if (map.has(complemento)) {
+      return [map.get(complemento), i];
+
+      //Vuelta 1
+      // num = 2
+      // complemento = 7
+      //map.has(7) = false
+      // map.set(num,i)=2,0)--guardamos en el map el 2 porque no es elcomplemento que buscamos
+
+      //Vuelta 2
+      // num = 7
+      // complemento = 2
+      // map.has(2) → ya visto en la vuelta 1, es true
+      // map.get(2) → 0
+    }
+
+    map.set(num, i); // guardamos el numero ya visto y el indicie
+  }
+}
+
+console.log(twoSum2(nums2, target2)); //  Llamada correcta
